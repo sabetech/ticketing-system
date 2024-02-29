897a66fa488129c064c4b3fe5c032e928d865261
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-02-29 15:57:47 +0000

    use Rate

--- app/Http/Controllers/API/RateController.php
+++ app/Http/Controllers/API/RateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use App\Rate;
 use App\Http\Controllers\API\BaseController as BaseController;
 
 class RateController extends BaseController
