36271bbc2c2f6e23415e54db9551d4291ba0a657
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-04-22 16:48:10 +0000

    modify fillable fields

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'lname', 'fname', 'email', 'password', 'username', 'photo', 'phone'
     ];
 
     /**
