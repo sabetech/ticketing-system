d7b6b19f3df3a6cf7d792e82ffd3f446ad1372d2
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-04-22 17:10:26 +0000

    set timestamps to false

--- app/ModelHasRoles.php
+++ app/ModelHasRoles.php
@@ -9,4 +9,5 @@ class ModelHasRoles extends Model
     //
     protected $table = 'model_has_roles';
     protected $fillable = ['role_id', 'model_type', 'model_id'];
+    public $timestamps = false;
 }
