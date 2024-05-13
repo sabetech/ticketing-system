73851b946f9e0b3f7673f6f8881a53e260adf58d
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-05-13 10:30:59 +0000

    migration for rate agent junction table

--- database/migrations/2024_05_10_124612_create_rate_agent_jxn_table.php
+++ database/migrations/2024_05_10_124612_create_rate_agent_jxn_table.php
@@ -15,7 +15,17 @@ class CreateRateAgentJxnTable extends Migration
     {
         Schema::create('rate_agent_jxn', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('agent_id');
+            $table->unsignedInteger('rate_id');
             $table->timestamps();
+
+            $table->foreign('agent_id')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('rate_id')
+                ->references('id')
+                ->on('rates_v2');
         });
     }
 
