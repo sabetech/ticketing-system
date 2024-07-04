bb53e3368dbd8e95644fceeb476aadcff1d11765
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-07-04 01:28:13 +0000

    Check this out

--- resources/views/pdf/taskforce.blade.php
+++ resources/views/pdf/taskforce.blade.php
@@ -2,15 +2,54 @@
 <html>
 <head>
 	<title>Taskforce Report</title>
+    <style>
+        /* Your PDF styling here */
+        .main {
+            margin: 1em
+        },
+        table {
+           border-collapse: collapse; /* Ensures borders between cells */
+        }
+        th, td {
+            border: 1px solid #ddd; /* Define 1px solid gray border for all cells */
+            padding: 5px; /* Add padding for readability */
+        }
+        .table-header {
+            background-color: #D3D3D3
+        }
+        .table {
+            border-color: #D3D3D3
+            border-radius: 5px
+            border-width: 1px
+        }
+        .row {
+            margin-bottom: 10px
+            border-width: 1px
+        }
+        .amount {
+            text-align: center,
+            width: '15%'
+        }
+        .date_time {
+            text-align: center,
+            width: '20%'
+        }
+        .qty_sold {
+            text-align: center
+        }
+
+    </style>
 </head>
 <body>
-	<h1>{{ $title }}</h1>
-    <?php
-        $totalAmount = 0;
-        $totalNumberOfTaskforceTickets = 0;
-    ?>
-    <table>
-        <thead>
+    <div class="main">
+        <h3 style="text-align: center">{{$title}}</h3>
+
+        <?php
+            $totalAmount = 0;
+            $totalNumberOfTaskforceTickets = 0;
+        ?>
+        <table >
+        <thead class="table-header">
           <tr>
             <th>Date Time Issued</th>
             <th>Car Number</th>
@@ -25,9 +64,9 @@
                 $totalNumberOfTaskforceTickets++;
             ?>
             <tr>
-              <td>{{ $taskforceInfo->issued_date_time }}</td>
+              <td class="date_time">{{ $taskforceInfo->issued_date_time }}</td>
               <td>{{ $taskforceInfo->car_number }}</td>
-              <td>{{ $taskforceInfo->amount }}</td>
+              <td class="amount">{{ $taskforceInfo->amount }}</td>
               <td>{{ $taskforceInfo->fname ." ". $taskforceInfo->lname}}</td>
             </tr>
           @endforeach
@@ -43,5 +82,6 @@
       </table>
 
 
-</body>
+    </div>
+    </body>
 </html>
