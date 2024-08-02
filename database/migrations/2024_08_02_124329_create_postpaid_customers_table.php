a36c71edf114122932fd61f8e846487d35ee16f8
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-08-02 14:12:48 +0000

    git commit migration to add postpaid customers table

--- database/migrations/2024_08_02_124329_create_postpaid_customers_table.php
+++ database/migrations/2024_08_02_124329_create_postpaid_customers_table.php
@@ -15,6 +15,11 @@ class CreatePostpaidCustomersTable extends Migration
     {
         Schema::create('postpaid_customers', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('station_id');
+            $table->unsignedInteger('rate_id');
+            $table->foreign('station_id')->references('id')->on('stations');
+            $table->foreign('rate_id')->references('id')->on('rates');
             $table->timestamps();
         });
     }
