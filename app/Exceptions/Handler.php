01aa38a259ba8fceac6adec55b9c81c5129f1ac9
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-08-08 05:32:17 +0000

    solve that constant OAuth error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,6 +34,10 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
+         // Kill reporting if this is an "access denied" (code 9) OAuthServerException.
+        if ($exception instanceof \League\OAuth2\Server\Exception\OAuthServerException && $exception->getCode() == 9) {
+            return;
+        }
         parent::report($exception);
     }
 
