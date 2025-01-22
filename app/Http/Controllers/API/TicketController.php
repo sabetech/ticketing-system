ad190154a8de26a93fb723392bab89f735d8bd67
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2025-01-22 16:31:46 +0000

    add eager loading to ticket load

--- app/Http/Controllers/API/TicketController.php
+++ app/Http/Controllers/API/TicketController.php
@@ -70,7 +70,7 @@ class TicketController extends BaseController {
             }
         });
 
-        $tickets = $tickets->with(['rate', 'agent'])->orderBy('created_at', 'desc')->paginate(50);
+        $tickets = $tickets->with(['rate', 'agent', 'station'])->orderBy('created_at', 'desc')->paginate(50);
 
         return $this->sendResponse($tickets, 'Tickets retrieved successfully');
 
