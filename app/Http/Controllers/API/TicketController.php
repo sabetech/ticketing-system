97a3bd062014b166135d67ce2cbde4d153bbeb0b
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2025-04-17 11:41:31 +0000

    Edit ticket test enable

--- app/Http/Controllers/API/TicketController.php
+++ app/Http/Controllers/API/TicketController.php
@@ -452,7 +452,7 @@ class TicketController extends BaseController {
             $ticket->agent = $agent->id;
         }
 
-        // $ticket->save();
+        $ticket->save();
 
         return $this->sendResponse($ticket, 'Ticket is Edited Successfully!');
     }
