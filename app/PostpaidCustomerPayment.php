306945dcc9a9ecba7c06c10a5759ea46e0c0bfaf
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2025-04-28 20:13:46 +0000

    [wip]

--- app/PostpaidCustomerPayment.php
+++ app/PostpaidCustomerPayment.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class PostpaidCustomerPayment extends Model
 {
     //
+    public static function SavePayment($dateRange, $rateTitle, $amount, $withholding_tax, $discount) {
+        $from = $dateRange[0];
+        $to = $dateRange[1];
+        PostpaidCustomerPayment::create([
+            'from' => $from,
+            'to' => $to,
+        ]);
+    }
 }
