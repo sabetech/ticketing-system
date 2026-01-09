efc49269332231954b0efde9db6e8b1837217623
aansah-mtn-gh <albert.mensahansah@mtn.com>
2026-01-09 16:39:04 +0000

    test where null impl

--- app/Http/Controllers/API/StationController.php
+++ app/Http/Controllers/API/StationController.php
@@ -24,8 +24,8 @@ class StationController extends BaseController
         $results = DB::table('toll_tickets')
         ->join('rates_v2', 'toll_tickets.rate_title', '=', 'rates_v2.id')
         ->join('stations', 'rates_v2.station_id', '=', 'stations.id')
-
         ->whereBetween('toll_tickets.issued_date_time', [$from, $to])
+        ->whereNull('toll_tickets.deleted_at')
         ->select('stations.id as station_id', 'stations.name','rates_v2.id as rate_id','rates_v2.title', 'rates_v2.icon', 'rates_v2.rate_type', 'rates_v2.is_postpaid', DB::raw('COUNT(*) as ticket_count'),DB::raw('SUM(toll_tickets.amount) as total_amount'))
         ->groupBy('stations.id', 'stations.name', 'rates_v2.id', 'rates_v2.title', 'rates_v2.icon', 'rates_v2.rate_type', 'rates_v2.is_postpaid')
         ->orderBy('stations.name')
